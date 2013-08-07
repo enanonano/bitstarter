ec6370cf8fd8b0e0267fc48acf099aa91ff6ffb4
alejandro <enano_nano@yahoo.com>
2013-08-07 20:03:15 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var content = fs.readFileSync(index.html);
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2 Alejandro you are awsom.. You are failing. vio te amoe');
+ // response.send('Hello World! 2 Alejandro you are awsom.. You are failing. vio te amoe');
+esponse.send(content);
 });
 
 var port = process.env.PORT || 5000;
