8fa07baf259ae15f603f993e380d2d723b73480f
alejandro <enano_nano@yahoo.com>
2013-08-07 20:39:56 +0000

    one more

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
-var content = fs.readFileSync(index.html);
+var content = "Alex";
+fs = require('fs');
+fs.readFile('/etc/hosts', 'utf8', function (err,data) {
+  if (err) {
+    //return console.log(err);
+ content = err;
+
+  }
+  //console.log(data);
+content=data;
+});
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World! 2 Alejandro you are awsom.. You are failing. vio te amoe');
-esponse.send(content);
+response.send(content);
 });
 
 var port = process.env.PORT || 5000;
